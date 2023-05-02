d8a8c865ae68c79c9fd1a56834af7d3573bad6aa
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-05-02 08:40:10 +0530

    feat: setup event handlers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let fileList: File[] = []
+
+  function handleFileChange(event: Event) {
+    const { files } = event.target as HTMLInputElement
+    if (files) {
+      fileList = Array.from(files)
+    }
+  }
+</script>
+
+<svelte:head>
+  <title>File Drop</title>
+  <meta
+    name="description"
+    content="FileDrop is a user-friendly web app that simplifies file sharing and uploading with a sleek drag-and-drop interface, multiple file uploads, and encryption for secure transfers."
+  />
+</svelte:head>
+
+<h1>Upload your files</h1>
+
+<hr />
+<div
+  on:dragover|preventDefault
+  on:drop|preventDefault={(e) => {
+    const draggedData = e.dataTransfer
+    if (draggedData) {
+      fileList = Array.from(draggedData.files)
+    }
+  }}
+>
+  <label for="file">
+    <div>
+      <svg
+        aria-hidden="true"
+        fill="none"
+        stroke="currentColor"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+        />
+      </svg>
+      <p>Click to upload or drag and drop</p>
+    </div>
+    <input hidden id="file" type="file" multiple on:change={handleFileChange} />
+  </label>
+</div>
+<hr />
+
+<h2>Files Selected: {fileList.length}</h2>
+{#if fileList.length}
+  <ul>
+    {#each fileList as file}
+      <li>
+        {file.name}
+      </li>
+    {/each}
+  </ul>
+{/if}
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +7,6 @@ const config = {
   kit: {
     adapter: adapter()
   }
-};
+}
 
-export default config;
+export default config
