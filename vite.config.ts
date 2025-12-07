e39334688560033bc82b30d090e9c9089367ef19
Abhishek Rawat <abhi.rawat456@gmail.com>
2025-12-07 14:00:21 +0530

    chore: revert polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { sveltekit } from '@sveltejs/kit/vite';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	optimizeDeps: {
-		esbuildOptions: {
-			define: { global: 'globalThis' },
-			plugins: [NodeGlobalsPolyfillPlugin({ buffer: true, process: true })],
-		},
-	},
 	plugins: [sveltekit(), UnoCSS()],
 });
